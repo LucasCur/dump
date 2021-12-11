229f7e3846255674fd7a08eb16249253332a71d5
Lucas Curran <92184180+LucasCur@users.noreply.github.com>
2021-12-11 11:33:40 +0000

    Update python.py

--- python.py
+++ python.py
@@ -7,4 +7,14 @@ for i in range(1,8):
 avg = total / 7
 print(avg)
 
-#
+#   ISOCELES TRIANGLE TEST   #
+
+Length1 = float(input("Length 1: "))
+Length2 = float(input("Length 2: "))
+Length3 = float(input("Length 3: "))
+if Length1 == Length2 or Length1 == Length3 or Length2 == Length3:
+  print("Isoceles")
+else:
+  print("Not Isoceles")
+
+#   
