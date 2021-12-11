6dbd0fa4be78b48dcd12b7410dcf5f2e0339b4ac
Lucas Curran <92184180+LucasCur@users.noreply.github.com>
2021-12-11 11:35:07 +0000

    Update python.py

--- python.py
+++ python.py
@@ -17,4 +17,19 @@ if Length1 == Length2 or Length1 == Length3 or Length2 == Length3:
 else:
   print("Not Isoceles")
 
-#   
+#   MATHMATICAL OPERATORS   #
+
+addition = int(input("Add 1 to: ")) + 1
+print(addition)
+subtraction = int(input("Subtract 1 from: ")) - 1
+print(subtraction)
+multiplication = int(input("Multiply 10 by: ")) * 10
+print(multiplication)
+division1 = 5 / float(input("Divide 5: "))
+print(division1)
+division2 = 5 // int(input("Integer Divide 5: "))
+print(division2)
+remainder = int(input("Remainder of 7 and: ")) % 7
+print(remainder)
+exponential = int(input("Exponential of 3 and: ")) ** 3
+print(exponential)
