cdc60e01369b62645f4f2e96ae0230693d0b0817
Lucas Curran <92184180+LucasCur@users.noreply.github.com>
2021-12-11 12:01:02 +0000

    Update random.cs

--- C#/random.cs
+++ C#/random.cs
@@ -2,6 +2,18 @@
 
 public class Program 
 {
+    public static int Sum(int a, int b) => a + b;
+}
+
+public class Program {
+	public static int convert(int minutes) 
+	{
+		return minutes * 60;
+	}
+}
+
+public class Program 
+{
     public static bool LessThanOrEqualToZero(double a) 
     {
       return a <= 0;
